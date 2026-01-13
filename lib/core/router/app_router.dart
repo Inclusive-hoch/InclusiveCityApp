4b567979fafc8bb9612bd7182b479c686da555e0
d.toro05 <d.toro05@ufromail.cl>
2026-01-13 04:48:54 -0300

    create: app_router.dart

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:go_router/go_router.dart';
+import 'package:inclusive_app/core/router/auth_router_notifier.dart';
+import 'package:inclusive_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:inclusive_app/features/auth/presentation/bloc/auth_state.dart';
+import 'package:inclusive_app/features/auth/presentation/pages/auth_page.dart';
+import 'package:inclusive_app/features/auth/presentation/views/login_form.dart';
+import 'package:inclusive_app/features/map_view/presentation/map_page.dart';
+
+GoRouter createRouter(AuthBloc authBloc) {
+  return GoRouter(
+    initialLocation: '/login',
+    refreshListenable: AuthRouterNotifier(authBloc),
+
+    // redirect: (context, state) {
+    //   final authState = authBloc.state;
+    //   final isLogin = state.matchedLocation == '/login';
+
+    //   if (authState is AuthAuthenticated) {
+    //     return isLogin ? '/map' : null;
+    //   }
+
+    //   if (authState is AuthUnauthenticated) {
+    //     return isLogin ? null : '/login';
+    //   }
+
+    //   return null;
+    // },
+    routes: [
+      GoRoute(path: '/login', builder: (_, __) => const AuthPage()),
+      GoRoute(path: '/login_form', builder: (_, __) => const LoginForm()),
+      GoRoute(path: '/map', builder: (_, __) => const MapPage()),
+    ],
+  );
+}
