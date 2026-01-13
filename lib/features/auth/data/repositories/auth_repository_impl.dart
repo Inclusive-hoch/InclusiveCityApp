5e23bfd9d3ac1b34a91568e22bbe60e5fa1df4cf
d.toro05 <d.toro05@ufromail.cl>
2026-01-13 04:53:33 -0300

    create(auth_repository_impl): implementacion de autenticacion de firebase

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,32 @@
+import '../../domain/entities/user.dart';
+import '../../domain/repositories/auth_repository.dart';
+import '../datasources/auth_firebase_datasource.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthFirebaseDataSource firebase;
+
+  AuthRepositoryImpl(this.firebase);
+
+  @override
+  Future<User> loginWithEmail(String email, String password) async {
+    final userModel = await firebase.loginWithEmail(email, password);
+    return userModel.toEntity();
+  }
+
+  @override
+  Future<User> loginWithGoogle() async {
+    final userModel = await firebase.loginWithGoogle();
+    return userModel.toEntity();
+  }
+
+  @override
+  Future<User?> getCurrentUser() async {
+    final userModel = await firebase.getCurrentUser();
+    return userModel?.toEntity();
+  }
+
+  @override
+  Future<void> logout() async {
+    await firebase.logout();
+  }
+}
