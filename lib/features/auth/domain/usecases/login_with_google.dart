63ac9af1acfc574a61fc3b98beaac2a5f6cf08f0
d.toro05 <d.toro05@ufromail.cl>
2026-01-13 04:54:54 -0300

    create: caso de uso iniciar sesion con cuenta de google

---
+++ lib/features/auth/domain/usecases/login_with_google.dart
@@ -0,0 +1,12 @@
+import 'package:inclusive_app/features/auth/domain/entities/user.dart';
+import 'package:inclusive_app/features/auth/domain/repositories/auth_repository.dart';
+
+class LoginWithGoogle {
+  final AuthRepository repository;
+
+  LoginWithGoogle(this.repository);
+
+  Future<User> call() {
+    return repository.loginWithGoogle();
+  }
+}
