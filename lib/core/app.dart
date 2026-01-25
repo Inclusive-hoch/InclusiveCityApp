eb2a0d0612fc150cea6e0d73259baea0ac8a64ba
d.toro05 <d.toro05@ufromail.cl>
2026-01-25 16:55:24 -0300

    fix: agrega validacion de sesión al iniciar la aplicación

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:inclusive_app/core/router/app_router.dart';
 import 'package:inclusive_app/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:inclusive_app/features/auth/presentation/bloc/auth_event.dart';
 import 'package:inclusive_app/features/map_view/presentation/bloc/map_bloc.dart';
 import 'package:inclusive_app/features/places/presentation/bloc/place_bloc.dart';
 import 'package:inclusive_app/injection_container.dart' as di;
@@ -14,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<AuthBloc>(
-          create: (_) => di.sl<AuthBloc>()..add(AuthStarted()),
-        ),
+        BlocProvider<AuthBloc>(create: (_) => di.sl<AuthBloc>()),
         BlocProvider<MapBloc>(create: (_) => di.sl<MapBloc>()),
         BlocProvider(create: (context) => di.sl<MapBloc>()),
         BlocProvider(create: (context) => di.sl<PlaceBloc>()),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:inclusive_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:inclusive_app/features/auth/presentation/bloc/auth_event.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'injection_container.dart' as di;
 import 'core/app.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +13,7 @@ Future<void> main() async {
   await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
   await di.init();
-  //await di.sl<TempAuthService>().loginHardcoded();
+  di.sl<AuthBloc>().add(AuthStarted());
 
   runApp(const MyApp());
 }
