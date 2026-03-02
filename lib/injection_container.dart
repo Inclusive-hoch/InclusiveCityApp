fa38ef5058d9f06dd7507699a7f50cfb6abce3cc
BerAxz <bastiaxd@gmail.com>
2026-03-01 22:32:52 -0300

    refactor: update route_bloc import paths to reflect new application structure

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -57,7 +57,7 @@ import 'package:inclusive_app/features/routing/data/datasources/route_local_data
 import 'package:inclusive_app/features/routing/data/repositories/route_repository_impl.dart';
 import 'package:inclusive_app/features/routing/domain/repositories/route_repository.dart';
 import 'package:inclusive_app/features/routing/domain/usecases/get_alternative_route.dart';
-import 'package:inclusive_app/features/routing/presentation/bloc/route_bloc.dart';
+import 'package:inclusive_app/features/routing/application/bloc/route_bloc.dart';
 
 final sl = GetIt.instance;
 
