3b9d9ebc4136c9efde0d55eb484df67ce33adc8a
SebaS01010101 <s.sandoval18@ufromail.cl>
2026-03-10 21:53:24 -0300

    refactor: remove UserLocationLoaded state from PlacesState class

--- lib/features/places/presentation/bloc/place_state.dart
+++ lib/features/places/presentation/bloc/place_state.dart
@@ -52,20 +52,6 @@ class PlaceDetailsLoaded extends PlacesState {
   List<Object> get props => [placeDetails];
 }
 
-/// Estado cuando se ha obtenido la ubicación del usuario.
-class UserLocationLoaded extends PlacesState {
-  /// Latitud de la ubicación del usuario.
-  final double latitude;
-
-  /// Longitud de la ubicación del usuario.
-  final double longitude;
-
-  UserLocationLoaded(this.latitude, this.longitude);
-
-  @override
-  List<Object> get props => [latitude, longitude];
-}
-
 /// Estado cuando se ha cargado el historial de búsquedas.
 class SearchHistoryLoaded extends PlacesState {
   /// Lista de lugares del historial de búsquedas.
